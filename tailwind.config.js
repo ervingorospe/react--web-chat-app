aafcf6908d82251e9834d88f2b985216df84439b
ervin <ervingorospe.code@gmail.com>
2022-10-04 21:31:52 +0800

    created example tailwind themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        slate: colors.slate,
+        gray: colors.gray
+      },
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)'
+        }
+      },
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)',
+          'button-muted-hover': 'var(--color-button-muted-hover)'
+        }
+      },
+      gradientColorStops: {
+        skin: {
+          hue: 'var(--color-fill)'
+        }
+      }
+    },
   },
   plugins: [],
 }
